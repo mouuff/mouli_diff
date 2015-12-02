18d273320183f7441e3783d3262b781fa784e820
Arnaud Alies <arnaud.alies@epitech.eu>
2015-12-02 09:53:27 +0100

    ok

--- bsq_diff.sh
+++ bsq_diff.sh
@@ -2,9 +2,11 @@
 
 let "a = 0"
 
+s = "_"
+
 for bsq in bsqs/*
 do
-    ./$bsq tests/test_40 > $bsq$a
-    mv $bsq$a res/
+    ./$bsq tests/test_40 > $bsq$s$a
+    mv $bsq$s$a res/
     let "a = a + 1"
 done
