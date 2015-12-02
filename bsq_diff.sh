0f0731ef931270752eb6a10d8d39a581a4eff371
Arnaud Alies <arnaud.alies@epitech.eu>
2015-12-02 10:35:54 +0100

    ok

--- bsq_diff.sh
+++ bsq_diff.sh
@@ -4,14 +4,15 @@ let "a = 0"
 
 s="_"
 
-if [$1 == ""] then
-   echo "Usage: $0 [test file]"
-   exit 1
-if
+if [ -z "$1" ]
+then
+    echo "No argument supplied"
+    exit 1
+fi
 
 for bsq in bsqs/*
 do
-    ./$bsq $1 > $bsq$s$a
+    ./$bsq {$1} > $bsq$s$a
     mv $bsq$s$a res/
     let "a = a + 1"
 done
